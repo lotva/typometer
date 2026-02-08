1a28fec0d7a053f59e52a64cbf5fcfd9aaf1be95
lotva <dlotva@gmail.com>
2026-02-08 15:42:27 +0300

    fix: tests runner

--- app/views/index/IndexPage.spec.ts
+++ app/views/index/IndexPage.spec.ts
@@ -1,13 +1,11 @@
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import { describe, expect, it } from 'vitest'
 
-import SampleComponent from '../../common/ui/SampleComponent.vue'
 import IndexPage from './IndexPage.vue'
 
 describe('IndexPage', () => {
 	it('works with mountSuspended', async () => {
 		const wrapper = await mountSuspended(IndexPage)
-		const sampleComponent = wrapper.findComponent(SampleComponent)
-		expect(sampleComponent.exists()).toBeTruthy()
+		expect(wrapper.exists()).toBeTruthy()
 	})
 })
