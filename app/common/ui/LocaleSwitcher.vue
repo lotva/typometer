83a246086940f58d0052000fb9b7c19603a1c87c
lotva <dlotva@gmail.com>
2026-07-23 15:45:44 +0300

    feat: restyle locale switcher

--- app/common/ui/LocaleSwitcher.vue
+++ app/common/ui/LocaleSwitcher.vue
@@ -13,7 +13,17 @@
 			replace
 			@mouseover="preloadLocale(locale.code)"
 		>
-			<span class="label capitals text-metrics-fix">
+			<span
+				class="visually-hidden"
+				:lang="locale.code"
+			>
+				{{ $t(`languages.${locale.code}`) }}
+			</span>
+
+			<span
+				class="label capitals text-metrics-fix"
+				aria-hidden="true"
+			>
 				{{ locale.code.toUpperCase() }}
 			</span>
 		</SwitchLocalePathLink>
@@ -33,21 +43,28 @@
 
 <style scoped>
 	.switcher {
-		--width: 50%;
+		--width: calc(50% - var(--typography__outline-thickness));
 
 		display: flex;
+
+		padding: var(--typography__outline-thickness);
+		border-radius: calc(var(--radius) + var(--typography__outline-thickness));
+
+		font-size: var(--fs-s);
+
+		background-color: var(--color__muted);
 
 		&::before {
 			content: '';
 
 			position: absolute;
-			inset-block: 0;
+			inset-block: var(--typography__outline-thickness);
 			inset-inline-start: var(--left);
 
 			inline-size: var(--width);
 			border-radius: var(--radius);
 
-			background-color: var(--color__muted);
+			background-color: var(--color__background);
 
 			transition-timing-function: var(--animation__ease-in-out);
 			transition-duration: var(--animation__duration--fast);
@@ -59,25 +76,26 @@
 		}
 
 		&:has([data-selected='true'][data-locale='en']) {
-			--left: 0;
+			--left: var(--typography__outline-thickness);
 		}
 	}
 
 	:deep(.locale-link) {
 		padding: calc(
-				var(--gap) / 2 + var(--typography__surface-capital-compensator)
+				var(--gap) * 0.5 + var(--typography__surface-capital-compensator)
 			)
-			calc(var(--gap) * 1) calc(var(--gap) / 2);
+			calc(var(--gap) * 0.75) calc(var(--gap) * 0.5);
 		border-radius: var(--radius);
-		color: var(--color__foreground);
+		color: var(--color__foreground--muted);
 		text-decoration: none;
 
 		&[aria-current='page'] {
 			cursor: default;
+			color: var(--color__foreground);
 		}
 
 		&:not([aria-current='page']):hover {
-			color: var(--color__foreground--strong);
+			color: var(--color__foreground);
 		}
 	}
 
